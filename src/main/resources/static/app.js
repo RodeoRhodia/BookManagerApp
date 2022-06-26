b7daf6b89f9e561801721270fecc09485f331ab6
Lanz <lanzdagamer@gmail.com>
2022-06-26 16:33:45 -0400

    external js file, not in use

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const bookList = document.querySelector('#book-list ul');
+//
+//     bookList.addEventListener('click', function(event) {
+//         if (event.target.className == 'delete') {
+//             const bookItem = event.target.parentNode;
+//             console.log(bookItem);
+//
+//             // li.parentNode.removeChild(li);
+//         }
+//     });
+//
+//     console.log(bookObjects);
+// });
