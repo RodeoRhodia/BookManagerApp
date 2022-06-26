c45ab7e6027f917e0fef41cd73db3fbc0ec37f69
Lanz <lanzdagamer@gmail.com>
2022-06-26 15:12:29 -0400

    added get, post, delete functionality

--- src/main/java/com/sampleProject/bookManager/book/BookController.java
+++ src/main/java/com/sampleProject/bookManager/book/BookController.java
@@ -1,8 +1,7 @@
 package com.sampleProject.bookManager.book;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -10,12 +9,25 @@ import java.util.List;
 @RestController
 public class BookController {
 
-//    @GetMapping("home")
-//    public ModelAndView welcome() {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("home.html");
-//        return modelAndView;
-//    }
+    private BookService bookService;
 
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
+    @GetMapping("books")
+    public List<Book> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @PostMapping("books")
+    public void addBook(@RequestBody Book book) {
+        bookService.addBook(book);
+    }
+
+    @DeleteMapping("books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") Long bookId) {
+        bookService.deleteBook(bookId);
+    }
 }
